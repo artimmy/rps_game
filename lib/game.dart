442275a14c6b9a168df19dcf4b49f7467cef72df
artimmy <arthurtimmy@hotmail.com>
2022-01-26 20:35:34 -0300

    adding game.dart, game logic

--- lib/game.dart
+++ lib/game.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 class Game extends StatefulWidget 
@@ -10,6 +11,64 @@ class Game extends StatefulWidget
 
 class _GameState extends State<Game> 
 {
+
+  var _imageCpuChoice = const AssetImage("images/default.png");
+  String _textResult = "Choose an option below";
+  
+  void _selectedOption(int userChoice)
+  {
+    const options = [1,2,3];
+    int optionsIndex = Random().nextInt(3);
+    var cpuChoice = options[optionsIndex];
+    
+    switch (cpuChoice) 
+    {
+      case 1:
+        setState(() => _imageCpuChoice = const AssetImage("images/rock.png"));
+        break;
+      case 2:
+        setState(() => _imageCpuChoice = const AssetImage("images/paper.png"));
+        break;
+      case 3:
+        setState(() => _imageCpuChoice = const AssetImage("images/scissor.png"));
+        break;
+    }
+
+    // Winner Validation
+    //User Winner
+    if(
+        (userChoice == 1 && cpuChoice == 3) ||
+        (userChoice == 3 && cpuChoice == 2) ||
+        (userChoice == 2 && cpuChoice == 1)
+    )
+    {
+      setState(() {
+        _textResult = "Congrats!!! You Win :)";
+      });
+    
+    }
+    // Cpu Winner
+    else if(
+        (cpuChoice == 1 && userChoice == 3) ||
+        (cpuChoice == 3 && userChoice == 2) ||
+        (cpuChoice == 2 && userChoice == 1)
+    )
+    {
+      setState(() 
+      {
+        _textResult = "You Lose :(";
+      });
+    }
+    // Tie
+    else
+    {
+      setState(() 
+      {
+        _textResult = "We have a draw ;)";
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) 
   {
@@ -22,11 +81,8 @@ class _GameState extends State<Game>
       body: Center(
 
         child: Container(
-          padding: const EdgeInsets.all(16),
-          decoration: BoxDecoration(
-            border: Border.all(width: 5, color: Colors.amber),
-          ),
-          
+          padding: const EdgeInsets.all(15),
+
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
@@ -40,30 +96,45 @@ class _GameState extends State<Game>
                 ),
               ),
 
-              //image
-              const Text("Hand Emoji IMAGE><><><<><"),
+              // image
+              Image(image: _imageCpuChoice),
 
-              //text
-              const Text(
-                "You Win / You Lose", 
+              // text
+              Text(
+                _textResult, 
                 textAlign: TextAlign.center,
-                style: TextStyle(
+                style: const TextStyle(
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
                 ),
               ),
 
-              //row with 3 images
+              // row with 3 clickable images
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: const [
-                  Text("img1"),
-                  Text("img2"),
-                  Text("img3"),
-                ],
+                children: [
+                  
+                  InkWell(
+                    // When the user taps the button, show a snackbar.
+                    onTap: () => _selectedOption(1),
+                    child: Image.asset("images/rock.png", height: 100),
+                  ),
+                  InkWell(
+                    // When the user taps the button, show a snackbar.
+                    onTap: () => _selectedOption(2),
+                    child: Image.asset("images/paper.png", height: 100),
+                  ),
+                  InkWell(
+                    // When the user taps the button, show a snackbar.
+                    onTap: () => _selectedOption(3),
+                    child: Image.asset("images/scissor.png", height: 100),
+                  ),
+                   
+                ], // Row Children
+
               ),
 
-            ], //Children
+            ], // Children
 
           ),
 
